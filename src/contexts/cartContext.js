234d0da00603c56e342eec9df585f3a686cb9a7e
abdraimova09 <abdraimova09@gmail.com>
2022-07-01 14:36:50 +0600

    qwe

--- src/contexts/cartContext.js
+++ src/contexts/cartContext.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 export const cartContext = React.createContext();
 
 const INIT_STATE = {
   cart: null,
+  count: 0,
 };
 
 function reducer(state = INIT_STATE, action) {
@@ -12,6 +13,7 @@ function reducer(state = INIT_STATE, action) {
       return {
         ...state,
         cart: action.payload,
+        count: action.payload.products.length,
       };
     default:
       return state;
@@ -19,6 +21,7 @@ function reducer(state = INIT_STATE, action) {
 }
 
 const CartContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, INIT_STATE);
   function addToCart(product) {
     let cart = JSON.parse(localStorage.getItem("cart"));
     if (!cart) {
@@ -32,12 +35,73 @@ const CartContextProvider = ({ children }) => {
       count: 1,
       subPrice: product.price,
     };
-    cart.products.push(newProduct);
+
+    const isProductInCart = cart.products.some(
+      item => item.item.id === newProduct.item.id
+    );
+    // console.log("товар в корзине?", isProductInCart);
+    if (isProductInCart) {
+      cart.products = cart.products.filter(
+        item => item.item.id !== newProduct.item.id
+      );
+    } else {
+      cart.products.push(newProduct);
+    }
     localStorage.setItem("cart", JSON.stringify(cart));
-    console.log(cart);
+    // console.log(cart);
+    getCart();
+  }
+  function getCart() {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (!cart) {
+      cart = {
+        products: [],
+        totalPrice: 0,
+      };
+    }
+    dispatch({
+      type: "GET_CART",
+      payload: cart,
+    });
+  }
+  function deleteFromCart(id) {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (!cart) {
+      cart = {
+        products: [],
+        totalPrice: 0,
+      };
+    }
+    cart.products = cart.products.filter(item => item.item.id !== id);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    getCart();
+  }
+  //cart - корзина
+  //card - карточка
+  function changeCount(count, id) {
+    if (count <= 0) {
+      return;
+    }
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    cart.products = cart.products.map(item => {
+      if (item.item.id === id) {
+        item.count = count;
+      }
+      return item;
+    });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    getCart();
   }
   return (
-    <cartContext.Provider value={{ addToCart }}>
+    <cartContext.Provider
+      value={{
+        cart: state.cart,
+        count: state.count,
+        addToCart,
+        getCart,
+        deleteFromCart,
+        changeCount,
+      }}>
       {children}
     </cartContext.Provider>
   );
